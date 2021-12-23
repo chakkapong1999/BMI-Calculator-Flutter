1a7fcc0324b68be34e56bb3cfd81b8385d8778be
Chakkapong <chakkapong1999@gmail.com>
2021-12-23 14:03:03 +0700

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'BMI Calculator',
       theme: ThemeData(
         scaffoldBackgroundColor: Myconstant.light,
       ),
